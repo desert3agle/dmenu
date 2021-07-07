e52a151b1319df7e171b809870ec06fc40c9cfca
Harsh Vardhan <harsh.vardhan320@gmail.com>
2021-07-07 19:48:29 +0530

    border

--- config.def.h
+++ config.def.h
@@ -26,4 +26,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 5;
+static unsigned int border_width = 3;
